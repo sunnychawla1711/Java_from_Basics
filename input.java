583a3dde2a351dc9708a185c8f15dd248e2052a8
Sunny Chawla <chawla.sunny1711@gmail.com>
2026-06-05 21:27:42 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class input{
+    public static void main(String[] args) {
+        System.out.println("Enter your name:");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Your name is: "+name);
+
+    }
+}
