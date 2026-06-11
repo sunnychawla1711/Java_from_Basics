2a26c104ef1773c1af70eba55dd50c49d47cd265
Sunny Chawla <chawla.sunny1711@gmail.com>
2026-06-11 16:44:31 +0530

    Add files via upload

---
+++ greet.java
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import java.util.*;
+public class greet{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hey "+ name + " How are you! ");
+    }
+}
+
+
