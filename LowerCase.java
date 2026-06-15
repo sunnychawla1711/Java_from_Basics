f234a5353603a9c5fba803ebe209a6c681f5e5f7
Sunny Chawla <chawla.sunny1711@gmail.com>
2026-06-15 17:52:18 +0530

    Add files via upload

---
+++ LowerCase.java
@@ -0,0 +1,11 @@
+
+
+
+
+import java.util.*;
+public class LowerCase {
+    public static void main(String[] args){
+        String name  = "Sunny";
+        System.out.println(name.toLowerCase());
+    }
+}
