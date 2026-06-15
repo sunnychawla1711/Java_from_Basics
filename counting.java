76ea930f5bd80c1a255821ef64c56f6ca5863d2f
Sunny Chawla <chawla.sunny1711@gmail.com>
2026-06-15 17:56:17 +0530

    Add files via upload

---
+++ counting.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class counting{
+    public static void main(String[] args){
+      
+        // for(int i=1;i<=100;i++){
+        //     System.out.print(i);
+        // }
+
+
+
+
+        // int i=1;
+        // while(i<101){
+        //     System.out.print(i);
+        //     i++;
+        // }
+
+
+
+
+
+        // int i=1;
+        // do{
+        //     System.out.print(i);
+        //     i++;
+        // }while(i<101); 
+
+
+        
+
+        // for(int i=100;i>=1;i--){
+        //     System.out.print(i);
+        // }
+    }
+}
