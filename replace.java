cff521e8ebefe5fea8696d8fe095e18b8646aca3
Sunny Chawla <chawla.sunny1711@gmail.com>
2026-06-15 17:50:53 +0530

    Add files via upload

---
+++ replace.java
@@ -0,0 +1,11 @@
+
+
+
+
+import java.util.*;
+public class replace{
+    public static void main(String[] args){
+        String name  = "Sunny is a good boy";
+        System.out.println(name.replace(" ","_"));
+    }
+}
