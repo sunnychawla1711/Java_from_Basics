a39a7f2b73a7ae2effbd9dd205e8bc24c64fbe68
Sunny Chawla <chawla.sunny1711@gmail.com>
2026-06-16 18:08:26 +0530

    Add files via upload

---
+++ sumOfEven.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class sumOfEven {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n: ");
+        int n = sc.nextInt();
+        System.out.println("Sum of first "+ n + " Even numbers is "+ (n)*(n+1));
+
+    }
+}
